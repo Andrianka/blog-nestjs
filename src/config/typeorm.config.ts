2e99e915ef70a603db7505bbdbbd314094598390
andrianka <andrianka13@gmail.coms>
2020-12-08 08:00:28 +0100

    init

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres' as const,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  migrationsRun: true,
+  synchronize: true,
+};
